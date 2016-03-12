6ba51783f379ffbf3c40718dd5597983fc39fe13
wesleyserrano <wesleyserrano@gmail.com>
2016-03-12 12:41:42 -0300

    - Commit inicial:   - Peças posicionadas no tabuleiro;   - Scripts de seleção iniciados

---
+++ SoccerChess/Assets/Scripts/SelectPiece.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectPiece : MonoBehaviour
+{
+
+    [SerializeField]
+    Color objectColor;
+    [SerializeField]
+    SelectionManager selectionManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnMouseDown()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+    }
+
+    public void restoreColor()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = objectColor;
+    }
+}
---
+++ SoccerChess/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionManager : MonoBehaviour
+{
+    GameObject selectedPiece;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void selectNewPiece(GameObject piece)
+    {
+        if (selectedPiece != null)
+        {
+            selectedPiece.GetComponent<SelectPiece>().restoreColor();
+        }
+
+        selectedPiece = piece;
+    }
+}
